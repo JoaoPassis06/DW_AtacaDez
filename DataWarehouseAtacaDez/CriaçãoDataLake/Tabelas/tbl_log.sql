ac31d057c3d61c971c4d2b81b7cd91542b803736
JoaoPassis06 <pedrojassis06@gmail.com>
2026-04-21 12:46:27 -0300

    Atualizando projeto (Inserção de log de erros e sucesso e finalização da dimensão de tempo e compras)

---
+++ DataWarehouseAtacaDez/CriaçãoDataLake/Tabelas/tbl_log.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tbl_log]
+(
+	[data] DATETIME NOT NULL PRIMARY KEY, 
+    [mensagem] VARCHAR(255) NULL, 
+    [resultado] VARCHAR(32) NULL
+)
---
+++ DataWarehouseAtacaDez/CriaçãoDataLake/Tabelas/tbl_tempo.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tbl_tempo]
+(
+	[cod_dia] VARCHAR(25) NOT NULL PRIMARY KEY, 
+    [date] DATE NULL, 
+    [controle] BIT NULL
+)
